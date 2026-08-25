6bc87971d5d9f6b7d5887175a43771a8818dcfa3
pwmarcofounder-cpu <pwmarcofounder@gmail.com>
2026-08-25 13:00:30 +0530

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -2,7 +2,7 @@ export async function onRequest(context) {
   const { request } = context;
   const url = new URL(request.url);
   const proxyHost = url.hostname; 
-  const vercelHost = 'remix-of-marco-live-gamma.vercel.app';
+  const vercelHost = 'marco-dev-pw.vercel.app';
   const videoHost = 'rolexcoderz.com';
 
   // 1. Handle CORS Preflight
